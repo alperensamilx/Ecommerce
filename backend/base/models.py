93f438a522139d94894b313285ebabbda02b926b
Alperen Şamil İlmaz <71771072+alperensamilx@users.noreply.github.com>
2021-11-12 22:54:22 +0300

    fifth commit

--- backend/base/models.py
+++ backend/base/models.py
@@ -3,10 +3,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=64, null=True, blank=True)
-    # image =
+    image = models.ImageField(null=True, blank=True)
     brand = models.CharField(max_length=64, null=True, blank=True)
     category = models.CharField(max_length=64, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
