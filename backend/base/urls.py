364f2c27fada6c0b4055abe015d723e1762482a8
Alperen Şamil İlmaz <71771072+alperensamilx@users.noreply.github.com>
2021-11-14 23:57:11 +0300

    fifth commit

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.get_routes, name='routes'),
+
     path('products/', views.get_products, name='products'),
-    path('users/profile/', views.get_user_profile, name='users-profile'),
     path('products/<str:pk>/', views.get_product, name='product'),
+
+    path('users/', views.get_users, name='users'),
+    path('users/profile/', views.get_user_profile, name='users-profile'),
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
+from django.contrib.auth.models import User
 from .models import Product
 from .products import products
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
@@ -46,7 +47,9 @@ def get_user_profile(request):
     serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
 
+
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_users(request):
     users =User.objects.all()
     serializer = UserSerializer(users, many=True)
