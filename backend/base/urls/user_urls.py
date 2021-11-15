fb910ad546d23ed56945eec1b78bae9f2535288f
Alperen Şamil İlmaz <71771072+alperensamilx@users.noreply.github.com>
2021-11-16 00:51:39 +0300

    fifth commit

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -5,6 +5,7 @@ from base.views import user_views as views
 urlpatterns = [
     path('', views.get_users, name='users'),
     path('profile/', views.get_user_profile, name='users-profile'),
+    path('profile/update/', views.update_user_profile, name='user-profile-update'),
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/', views.register_user, name='register'),
 ]
